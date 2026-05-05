a7b9d020d824fa4154a951217db49ce8d3694cb4
Marco <marcopoma943@gmail.com>
2026-05-04 19:48:01 -0500

    Fix port para Render

--- app.py
+++ app.py
@@ -404,7 +404,8 @@ def api_subir_imagen_producto():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
 
 
 
